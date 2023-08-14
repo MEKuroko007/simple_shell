9407b98e96ecbc0656e90766280291ac2faf2994
ouailb <adomeyou@gmail.com>
2023-08-14 21:04:10 +0100

    create file main.c + main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGS 20
+extern char** environ;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#endif
